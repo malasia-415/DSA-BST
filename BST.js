490741e4b83969ffea97f5e8a672347cba2f81f8
Malasia Woods <55638544+malasia-415@users.noreply.github.com>
2020-06-07 20:09:44 -0700

    Create BST.js

---
+++ BST.js
@@ -0,0 +1,16 @@
+public class BST {
+
+	public TreeNode root;
+
+	public static class TreeNode {
+
+		public TreeNode left;
+		public TreeNode right;
+		public Object data;
+
+		public TreeNode(3,1,4,6,9,2,5,7) {
+			this.data = data;
+			left = right = null;
+		}
+	}
+}
